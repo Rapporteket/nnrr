f7db72583ed59af999f484de1deb5fea60f960ac
kevinthon <kevin.thon@gmail.com>
2023-02-02 12:43:29 +0100

    tilpasning kontainer

--- R/nnrrHentRegData.R
+++ R/nnrrHentRegData.R
@@ -12,25 +12,30 @@ nnrrHentRegData <- function(datoFra = '2017-01-01', datoTil = '2099-01-01') {
   registryName <- "nnrr"
   dbType <- "mysql"
 
-  # query <- paste0("SELECT
-  #                 FROM AlleVariablerNum INNER JOIN ForlopsOversikt
-  #                 ON AlleVariablerNum.ForlopsID = ForlopsOversikt.ForlopsID
-  #                 WHERE HovedDato >= \'", datoFra, "\' AND HovedDato <= \'", datoTil, "\' ")
-  # RegData <- rapbase::LoadRegData(registryName, query, dbType)
-
-  # pasientsvar_pre <- read.table('C:/GIT/data/nnrr/DataDump_MRS-PROD_Pasientskjema+for+behandling_2022-12-09_1116_red.csv',
-  #                               sep=';', header=T, stringsAsFactors = F, fileEncoding = "UTF-8-BOM")
-  pasientsvar_pre <- readr::read_csv2('C:/GIT/data/nnrr/DataDump_MRS-PROD_Pasientskjema+for+behandling_2022-12-09_1116_v2.csv')
-  legeskjema <- read.table('C:/GIT/data/nnrr/DataDump_MRS-PROD_Behandlerskjema_2022-12-09_1116.csv', sep=';',
-                           header=T, fileEncoding = 'UTF-8-BOM', stringsAsFactors = F)
-  pasientsvar_post <- read.table('C:/GIT/data/nnrr/DataDump_MRS-PROD_Pasientskjema+6+maneder+etter+behandling_2022-12-09_1116.csv',
-                                 sep=';', header=T, fileEncoding = 'UTF-8-BOM', stringsAsFactors = F)
-  pasientsvar_post2 <- read.table('C:/GIT/data/nnrr/DataDump_MRS-PROD_Pasientskjema+12+maneder+etter+behandling_2022-12-09_1116.csv',
-                                  sep=';', header=T, fileEncoding = 'UTF-8-BOM', stringsAsFactors = F)
+  pasientsvar_pre <-
+    readr::read_csv2(
+      '~/data/nnrr/DataDump_MRS-PROD_Pasientskjema+før+behandling_2023-02-02_0917.csv')
+  legeskjema <-
+    readr::read_csv2(
+      '~/data/nnrr/DataDump_MRS-PROD_Behandlerskjema_2023-02-02_0917.csv')
+  pasientsvar_post <-
+    readr::read_csv2(
+      '~/data/nnrr/DataDump_MRS-PROD_Pasientskjema+6+måneder+etter+behandling_2023-02-02_0917.csv')
+  pasientsvar_post2 <-
+    readr::read_csv2(
+      '~/data/nnrr/DataDump_MRS-PROD_Pasientskjema+12+måneder+etter+behandling_2023-02-02_0918.csv')
 
   flere_hovedskjemaGuid <- names(table(pasientsvar_pre$HovedskjemaGUID))[table(pasientsvar_pre$HovedskjemaGUID)>1]
   if (!is.null(flere_hovedskjemaGuid)){
     pasientsvar_pre <- pasientsvar_pre[!(pasientsvar_pre$HovedskjemaGUID %in% flere_hovedskjemaGuid), ]
+  }
+  flere_hovedskjemaGuid <- names(table(pasientsvar_post$HovedskjemaGUID))[table(pasientsvar_post$HovedskjemaGUID)>1]
+  if (!is.null(flere_hovedskjemaGuid)){
+    pasientsvar_post <- pasientsvar_post[!(pasientsvar_post$HovedskjemaGUID %in% flere_hovedskjemaGuid), ]
+  }
+  flere_hovedskjemaGuid <- names(table(pasientsvar_post2$HovedskjemaGUID))[table(pasientsvar_post2$HovedskjemaGUID)>1]
+  if (!is.null(flere_hovedskjemaGuid)){
+    pasientsvar_post2 <- pasientsvar_post2[!(pasientsvar_post2$HovedskjemaGUID %in% flere_hovedskjemaGuid), ]
   }
 
   # icd10 <- read.table('C:/GIT/data/nnrr/icd10.csv', sep=';',
@@ -45,8 +50,7 @@ nnrrHentRegData <- function(datoFra = '2017-01-01', datoTil = '2099-01-01') {
   names(pasientsvar_post2)[names(pasientsvar_post2)=='SkjemaGUID'] <- 'SkjemaGUID_post2'
 
   RegData <- merge(legeskjema, pasientsvar_pre, by.x = 'SkjemaGUID',
-                   by.y = 'HovedskjemaGUID', suffixes = c('', '_pre'),
-                   all.x = TRUE)
+                   by.y = 'HovedskjemaGUID', suffixes = c('', '_pre'))
   RegData <- merge(RegData, pasientsvar_post, by.x = 'SkjemaGUID',
                    by.y = 'HovedskjemaGUID', suffixes = c('', '_post'),
                    all.x = TRUE)
