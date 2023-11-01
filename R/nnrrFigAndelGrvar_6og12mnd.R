95476f4b9879bc43dfc5f2e1f3d257d51b9dc1e9
kevinthon <kevin.thon@gmail.com>
2023-11-01 16:06:41 +0100

    slå sammen 6 og 12

--- R/nnrrFigAndelGrvar_6og12mnd.R
+++ R/nnrrFigAndelGrvar_6og12mnd.R
@@ -8,7 +8,8 @@
 #'
 #' @export
 #'
-nnrrFigAndelGrvar <- function(RegData, valgtVar="opplevd_nytte_beh_6mnd",
+nnrrFigAndelGrvar_6og12mnd <- function(RegData, valgtVar1="opplevd_nytte_beh_6mnd",
+                              valgtVar2="opplevd_nytte_beh_12mnd",
                               datoFra='2014-01-01', datoTil='2050-12-31',
                               enhetsUtvalg=1, datovar="Besoksdato",
                               minald=0, maxald=130, erMann=99, outfile='',
@@ -16,10 +17,10 @@ nnrrFigAndelGrvar <- function(RegData, valgtVar="opplevd_nytte_beh_6mnd",
                               grtittel="Behandling i spesialisthelsetjenesten")
 {
 
-  valgtVar="opplevd_nytte_beh_6mnd"; valgtVar="opplevd_nytte_beh_12mnd";
-  datoFra='2022-10-01'; datoTil='2023-09-30';
-  enhetsUtvalg=1; datovar="dato_oppfolg";
-  minald=0; maxald=130; erMann=99; outfile=''; inkl_konf=0; grvar="beh_spes_v3"
+  # valgtVar1="opplevd_nytte_beh_6mnd"; valgtVar2="opplevd_nytte_beh_12mnd";
+  # datoFra='2022-10-01'; datoTil='2023-09-30';
+  # enhetsUtvalg=1; datovar="dato_oppfolg"; grtittel="Behandling i spesialisthelsetjenesten";
+  # minald=0; maxald=130; erMann=99; outfile=''; inkl_konf=0; grvar="beh_spes_v3"
 
   RegData <- RegData[!is.na(RegData[, grvar]), ] # Forkast registreringer hvor grupperingsvariabel ikke finnes
   RegData$grvar <- RegData[, grvar]
@@ -35,19 +36,19 @@ nnrrFigAndelGrvar <- function(RegData, valgtVar="opplevd_nytte_beh_6mnd",
   }
 
   ## Preparer variabler for fremstilling i figur
-  PlotParams <- nnrrPrepVar(RegData=RegData, valgtVar=valgtVar)
-  RegData <- PlotParams$RegData
-  PlotParams$RegData <- NA
+  PlotParams1 <- nnrrPrepVar(RegData=RegData, valgtVar=valgtVar1)
+  PlotParams2 <- nnrrPrepVar(RegData=RegData, valgtVar=valgtVar2)
 
+  RegData <- PlotParams1$RegData
+  PlotParams1$RegData <- NA
   ## Gjør utvalg basert på brukervalg (LibUtvalg)
   NNRRUtvalg <- nnrrUtvalg(RegData=RegData, datoFra=datoFra, datoTil=datoTil, minald=minald,
-                           maxald=maxald, erMann=erMann, datovar=datovar)
+                           maxald=maxald, erMann=erMann, datovar="dato_oppfolg")
   RegData <- NNRRUtvalg$RegData
   utvalgTxt <- NNRRUtvalg$utvalgTxt
-
+  utvalgTxt[1] <- substr(utvalgTxt[1], 6, nchar(utvalgTxt[1]))
 
   # Initialiserer nødvendige størrelser
-
   ind <- list(Hoved=which(RegData$UnitId == reshID), Rest=which(RegData$UnitId != reshID))
 
   if (enhetsUtvalg==1) {
@@ -72,10 +73,44 @@ nnrrFigAndelGrvar <- function(RegData, valgtVar="opplevd_nytte_beh_6mnd",
                        Andel = Antall/N*100)
   }
 
+  RegData <- PlotParams2$RegData
+  PlotParams2$RegData <- NA
+  ## Gjør utvalg basert på brukervalg (LibUtvalg)
+  NNRRUtvalg <- nnrrUtvalg(RegData=RegData, datoFra=datoFra, datoTil=datoTil, minald=minald,
+                           maxald=maxald, erMann=erMann, datovar="dato_oppfolg2")
+  RegData <- NNRRUtvalg$RegData
+
+  # Initialiserer nødvendige størrelser
+  ind <- list(Hoved=which(RegData$UnitId == reshID), Rest=which(RegData$UnitId != reshID))
+
+  if (enhetsUtvalg==1) {
+    tabell_hoved2 <- RegData[ind$Hoved, ] %>%
+      dplyr::group_by(grvar, .drop = FALSE) %>%
+      dplyr::summarise(Antall = sum(Variabel),
+                       N = dplyr::n(),
+                       Andel = Antall/N*100)
+    tabell_rest2 <- RegData[ind$Rest, ] %>%
+      dplyr::group_by(grvar, .drop = FALSE) %>%
+      dplyr::summarise(Antall = sum(Variabel),
+                       N = dplyr::n(),
+                       Andel = Antall/N*100)
+    tabell_hoved2$Andel[tabell_hoved2$N == 0] <- 0
+    tabell_rest2$Andel[tabell_rest2$N == 0] <- 0
+    plottab2 <- dplyr::tibble(hoved=tabell_hoved2$Andel, rest=tabell_rest2$Andel)
+  } else {
+    tabell_hoved2 <- RegData %>%
+      dplyr::group_by(grvar, .drop = FALSE) %>%
+      dplyr::summarise(Antall = sum(Variabel),
+                       N = dplyr::n(),
+                       Andel = Antall/N*100)
+  }
+
+  plottab <- dplyr::bind_cols(plottab2, plottab)
+
   #-----------Figur---------------------------------------
 
-  tittel <- PlotParams$tittel; grtxt <- as.character(tabell_hoved[[1]]); grtxt2 <- PlotParams$grtxt2;
-  subtxt <- PlotParams$subtxt; cexgr <- PlotParams$cexgr; retn <- "H" #retn <- PlotParams$retn;
+  tittel <- PlotParams1$tittel; grtxt <- as.character(tabell_hoved[[1]]); grtxt2 <- PlotParams1$grtxt2;
+  subtxt <- PlotParams1$subtxt; cexgr <- PlotParams1$cexgr; retn <- "H" #retn <- PlotParams1$retn;
   FigTypUt <- rapFigurer::figtype(outfile=outfile, fargepalett='BlaaOff')
 
   NutvTxt <- length(utvalgTxt)
@@ -86,24 +121,28 @@ nnrrFigAndelGrvar <- function(RegData, valgtVar="opplevd_nytte_beh_6mnd",
   fargeHoved <- farger[1]
   fargeRest <- farger[3]
 
-  ymax <- 3*length(grtxt)*1.2
+  ymax <- 5*length(grtxt)*1.2
 
   if (enhetsUtvalg==1) {
     pos <- barplot(t(as.matrix(plottab)), beside = T, horiz = T,
-                   col = farger[c(3,4)], border = NA, ylim = c(0, ymax),
+                   col = c("red", "orange", farger[c(3,4)]), border = NA, ylim = c(0, ymax),
                    xlab = "Andel (%)")
 
-    legend('top', rev(c(paste0(shtxt, ", N=", sum(tabell_hoved$N)),
-                        paste0("Landet for øvrig", ", N=", sum(tabell_rest$N)))),
-           bty='n', fill=rev(farger[c(3,4)]), border=NA, ncol=1, cex=1, xpd = T)
+    legend('top', rev(c(paste0(shtxt, " 12 mnd, N=", sum(tabell_hoved2$N)),
+                        paste0("Landet for øvrig", " 12 mnd, N=", sum(tabell_rest2$N)),
+                        paste0(shtxt, " 6 mnd, N=", sum(tabell_hoved$N)),
+                        paste0("Landet for øvrig", " 6 mnd, N=", sum(tabell_rest$N)))),
+           bty='n', fill=rev(c("red", "orange", farger[c(3,4)])), border=NA, ncol=2, cex=1, xpd = T)
   }
 
-  paste0(tabell_hoved$Antall, " av ", tabell_hoved$N)
-  text(x=0, y=pos[1,], labels = paste0(tabell_hoved$Antall, " av ", tabell_hoved$N), cex = .8, adj=0)
-  text(x=0, y=pos[2,], labels = paste0(tabell_rest$Antall, " av ", tabell_rest$N), cex = .8, adj=0)
+  # paste0(tabell_hoved$Antall, " av ", tabell_hoved$N)
+  text(x=0, y=pos[1,], labels = paste0(tabell_hoved2$Antall, " av ", tabell_hoved2$N), cex = .6, adj=0)
+  text(x=0, y=pos[2,], labels = paste0(tabell_rest2$Antall, " av ", tabell_rest2$N), cex = .6, adj=0)
+  text(x=0, y=pos[3,], labels = paste0(tabell_hoved$Antall, " av ", tabell_hoved$N), cex = .6, adj=0)
+  text(x=0, y=pos[4,], labels = paste0(tabell_rest$Antall, " av ", tabell_rest$N), cex = .6, adj=0)
   mtext(grtittel, WEST<-2, line=-1, cex=1, outer=TRUE)
   mtext(at=colMeans(pos)+0.05, text=grtxt, side=2, las=1, cex=1, adj=1, line=0.25)
-  title(tittel, font.main=1.2)	#line=0.5,
+  title(tittel, font.main=1.4)	#line=0.5,
   mtext(utvalgTxt, side=3, las=1, cex=0.9, adj=0, col=farger[1], line=c(3+0.8*((NutvTxt-1):0)))
 
   par('fig'=c(0, 1, 0, 1))
