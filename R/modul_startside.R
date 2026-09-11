7c0ea3f07e54b4bbc090acfb9cd20a26b8a5d2e3
Thon Kevin Otto <Kevin.Otto.Thon@helse-nord.no>
2026-09-11 09:04:00 +0200

    vnr

--- R/modul_startside.R
+++ R/modul_startside.R
@@ -11,7 +11,7 @@ startside_UI <- function(id) {
         class = "panel panel-default",
         div(
           class = "panel-heading", style = "background-color : #E0E0E0 ",
-          h2("Velkommen til Rapporteket for Norsk nakke- og ryggregister - debug", align = "center")
+          h2("Velkommen til Rapporteket for Norsk nakke- og ryggregister", align = "center")
         ),
         div(
           class = "panel-body", style = "background-color:#F0F0F0",
